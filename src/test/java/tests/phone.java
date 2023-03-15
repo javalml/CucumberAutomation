ac8106fd9d6c032b3d3cc37e9ce1eb68261b29fb
javalml <botirovnaa@gmail.com>
2023-03-15 12:38:28 -0500

    Initial commit

---
+++ src/test/java/tests/phone.java
@@ -0,0 +1,11 @@
+package tests;
+
+import utilities.Driver;
+
+public class phone {
+    public static void main(String[] args) {
+        Driver.getDriver().get("https://www.google.com");
+
+//        https://github.com/javalml/CucumberAutomation.git
+    }
+}
